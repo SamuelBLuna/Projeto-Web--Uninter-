e5989326781f158c75a590be996c1a96cd40ea4b
SamuelBLuna <samuhluna@gmail.com>
2023-10-16 21:08:55 -0300

    implementação do JS Atualizações

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
-let nomes = ['patricia', 'camila', 'mateus', 'samuel']
+const btnContato = document.getElementById('contact')
+const btnModal = document.querySelector('.close')
+const btnEnviar = document.getElementById('enviar')
+const container = document.querySelector('.container')
+const classContato = document.querySelector('.contato')
 
-console.log(nomes.indexOf('patricia'))
+btnContato.addEventListener('click', openModal)
+btnModal.addEventListener('click', closeModal)
+btnEnviar.addEventListener('click', meuForm)
+
+function openModal() {
+    container.classList.toggle('hiden')
+    classContato.classList.toggle('hiden')
+}
+function closeModal() {
+    container.classList.toggle('hiden')
+    classContato.classList.toggle('hiden')
+}
+
+function meuForm(e) {
+    e.preventDefault()
+    const nome = document.querySelector('#nome')
+    const email = document.querySelector('#email')
+    const assunto = document.querySelector('#assunto')
+    const mensagem = document.querySelector('#mensagem')
+    let itsOk = confirm(
+        `
+        Verifique se os dados estão corretos
+
+        Nome: ${nome.value}
+        E-mail: ${email.value}`
+    )
+
+    if(itsOk) {
+        alert('Muito obrigado por entrar em contato')
+    }
+    nome.value = ''
+    email.value = ''
+    assunto.value = ''
+    mensagem.value = ''
+}
